22bef684ebf85a92d7d668369f7c507039d68756
Vladislav <vladmos1995@gmail.com>
2022-12-14 11:51:10 +0300

    Create projectsList.js

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,42 @@
+import project_01 from "./../img/projects/01.jpg";
+import project_01_Big from "./../img/projects/01-big.jpg";
+
+import project_02 from "./../img/projects/02.jpg";
+import project_02_Big from "./../img/projects/02-big.jpg";
+
+const projects = [
+   {
+      id:1,
+      title: 'Gaming streaming portal',
+      skills: 'React, Node.JS, MongoDB',
+      img: project_01,
+      imgBig: project_01_Big,
+      gitHubLink: 'http://github.com/'
+   },
+   {
+      id:2,
+      title: 'Video service',
+      skills: 'React, Node.JS, MongoDB',
+      img: project_02,
+      imgBig: project_02_Big,
+      gitHubLink: 'http://github.com/'
+   },
+   {
+      id:3,
+      title: 'Video service',
+      skills: 'React, Node.JS, MongoDB',
+      img: project_02,
+      imgBig: project_02_Big,
+      gitHubLink: 'http://github.com/'
+   },
+   {
+      id:4,
+      title: 'Video service',
+      skills: 'React, Node.JS, MongoDB',
+      img: project_02,
+      imgBig: project_02_Big,
+      gitHubLink: 'http://github.com/'
+   },
+];
+
+export { projects};
