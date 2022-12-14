b55c9c94381a90f7631ed54627130fe51be43972
Vladislav <vladmos1995@gmail.com>
2022-12-14 11:52:17 +0300

    Create NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <main className="section">
+      <div className="container">
+         <h1 className="title-1">Page Not Found</h1>
+      </div>
+   </main>
+  )
+}
+
+export default NotFoundPage
