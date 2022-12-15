930fdfa1935df25fb84f772a28ada23c61bef06d
Vladislav <vladmos1995@gmail.com>
2022-12-15 16:54:24 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-import cl from "./Footer.module.css";
+// styles
+import "./Footer.css";
 
 // icons - social buttons
 import vk from "./icons/vk.svg";
@@ -11,18 +12,18 @@ import linkedIn from "./icons/linkedIn.svg";
 
 const Footer = () => {
   return (
-   <footer className={cl.footer}>
-      <div className={cl.container}>
-         <div className={cl.footer__wrapper}>
-            <ul className={cl.social}>
-                  <li className={cl.social__item}><a href="#!"><img src={vk} alt="Link"/></a></li>
-                  <li className={cl.social__item}><a href="#!"><img src={instagram} alt="Link"/></a></li>
-                  <li className={cl.social__item}><a href="#!"><img src={twitter} alt="Link"/></a></li>
-                  <li className={cl.social__item}><a href="#!"><img src={gitHub} alt="Link"/></a></li>
-                  <li className={cl.social__item}><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
+   <footer className="footer">
+      <div className="container">
+         <div className="footer__wrapper">
+            <ul className="social">
+               <li className="social__item"><a href="#!"><img src={vk} alt="Link"/></a></li>
+               <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
+               <li className="social__item"><a href="#!"><img src={twitter} alt="Link"/></a></li>
+               <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
+               <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
             </ul>
-            <div className={cl.copyright}>
-                  <p>©</p>
+            <div className="copyright">
+               <p>©</p>
             </div>
          </div>
       </div>
