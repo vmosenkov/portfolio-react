e75e8b9405698803b32fb9dca307a88380a67ede
Vladislav <vladmos1995@gmail.com>
2022-12-15 16:54:12 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,26 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 // pages
 import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
-import ProjectPage from './pages/ProjectPage';
+import ProjectsPage from './pages/Project/ProjectsPage/ProjectsPage';
+import ProjectPage from './pages/Project/ProjectPage/ProjectPage';
 import ContactsPage from './pages/ContactsPage';
 import NotFoundPage from './pages/NotFoundPage';
+
+// utils
+import ScrollToTop from './utils/scrollToTop.js'
 
 function App() {
    return (
       <div className="App">
          <Router>
+            <ScrollToTop />
             <Navbar />
             <Routes>
                <Route path="/" element={<HomePage />} />
                <Route path="/projects" element={<ProjectsPage />} />
-               <Route path="/project" element={<ProjectPage />} />
+               <Route path="/project/:id" element={<ProjectPage />} />
                <Route path="/contacts" element={<ContactsPage />} />
-               {/* <Route path="/projects/:id" element={<ProjectPage />} /> */}
+               <Route path="*" element={<NotFoundPage />} />
             </Routes>
          </Router>
          <Footer />
