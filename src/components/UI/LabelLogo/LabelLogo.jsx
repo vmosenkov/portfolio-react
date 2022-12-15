58236fcce469c6afeaef7b97ff1ed2bb25a1dd65
Vladislav <vladmos1995@gmail.com>
2022-12-15 16:55:27 +0300

    Create LabelLogo.jsx

---
+++ src/components/UI/LabelLogo/LabelLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// label - logo
+import vmosenkovLabel_white from "./img/vmosenkovLabel_white.png";
+
+const LabelLogo = () => {
+   return (
+      <div>
+         <img src={vmosenkovLabel_white} alt="vmosenkov logo"/>
+      </div>
+   )
+}
+
+export default LabelLogo
