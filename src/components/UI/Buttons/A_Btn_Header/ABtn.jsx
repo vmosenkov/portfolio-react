e76c1e2d8363c0395c4638c7222d7a2c6d46e15d
Vladislav <vladmos1995@gmail.com>
2022-12-15 16:55:17 +0300

    Update ABtn.jsx

--- src/components/UI/Buttons/A_Btn_Header/ABtn.jsx
+++ src/components/UI/Buttons/A_Btn_Header/ABtn.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import cl from './ABtn.module.css'
+// styles
+import './ABtn.css'
 
 const ABtn = ({children, ...props}) => {
   return (
-    <a {...props} className={cl.btn}>{children}</a>
+    <a {...props} className="btn">{children}</a>
   )
 }
 
