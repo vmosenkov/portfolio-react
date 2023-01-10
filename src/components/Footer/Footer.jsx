c7953c63db7cbd2836d2decc69e0b702b8ad1ad3
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:50:56 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+// dependencies - icon
+import { BsInstagram } from 'react-icons/bs';
+import { BsTwitter } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaArtstation } from 'react-icons/fa';
+
 // styles
 import "./Footer.css";
 
 // icons - social buttons
 import vk from "./icons/vk.svg";
-import instagram from "./icons/instagram.svg";
-import twitter from "./icons/twitter.svg";
-import gitHub from "./icons/gitHub.svg";
-import linkedIn from "./icons/linkedIn.svg";
 
 const Footer = () => {
   return (
@@ -16,16 +19,19 @@ const Footer = () => {
       <div className="container">
          <div className="footer__wrapper">
             <ul className="social">
-               <li className="social__item"><a href="#!"><img src={vk} alt="Link"/></a></li>
-               <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
-               <li className="social__item"><a href="#!"><img src={twitter} alt="Link"/></a></li>
-               <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
-               <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
+               <li className="social__item"><a href="https://vk.com/vmosenkov" rel="noreferrer" target="_blank"><img src={vk} alt="Link"/></a></li>
+               <li className="social__item"><a href="https://www.instagram.com/v.mosenkov/" rel="noreferrer" target="_blank"><BsInstagram className="social__icons" /></a></li>
+               <li className="social__item"><a href="#!" rel="noreferrer" target="_blank"><BsTwitter className="social__icons" /></a></li>
+               <li className="social__item"><a href="https://github.com/vmosenkov" rel="noreferrer" target="_blank"><BsGithub className="social__icons" /></a></li>
+               <li className="social__item"><a href="www.linkedin.com/in/vmosenkov" rel="noreferrer" target="_blank"><BsLinkedin className="social__icons" /></a></li>
+               <li className="social__item"><a href="https://www.artstation.com/vmosenkov" rel="noreferrer" target="_blank"><FaArtstation className="social__icons" /></a></li>
             </ul>
             <div className="copyright">
-               <p>©</p>
+               {/* <p>All content copyright Vladislav Mosenkov</p> */}
             </div>
+            
          </div>
+         
       </div>
    </footer>
   )
