abe4f6e6f0fae8305297606a17c8ee5a0b15aeab
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:14 +0300

    Create CapCutSkillCard.jsx

---
+++ src/components/UI/SkillsCard/OtherPrograms/CapCutSkillCard/CapCutSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoCapCut from './logo-capcut.png'
+
+const CapCutSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+         <div className="skillCard">
+            <img className="skillCard-icon" src={logoCapCut} alt="logo react" />
+            <div className="skillCard-name">CapCut</div>
+         </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default CapCutSkillCard
