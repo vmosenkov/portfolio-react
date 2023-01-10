8428aa03d87a0a39d9d340f543217bfbee607363
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:21 +0300

    Create GitSkillCard.jsx

---
+++ src/components/UI/SkillsCard/OtherPrograms/GitSkillCard/GitSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoGit from './logo-git.svg'
+
+const GitSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+         <div className="skillCard">
+            <img className="skillCard-icon" src={logoGit} alt="logo react" />
+            <div className="skillCard-name">Git</div>
+         </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default GitSkillCard
