d232899dbb031055dbd16f1afba8071203b73b2b
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:55:35 +0300

    Create ReactIconSkillCard.jsx

---
+++ src/components/UI/SkillsCard/DependenciesSkills/ReactIconSkillCard/ReactIconSkillCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import logoReactIcon from './logo-react_icons.svg'
+
+const ReactIconSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+        <div className="skillCard">
+         <img className="skillCard-icon" src={logoReactIcon} alt="logo react" />
+         <div className="skillCard-name">React-icon</div>
+        </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default ReactIconSkillCard
