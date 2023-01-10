649f6c3ef1f3953b3c283f4d0b534437a9e9aea1
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:55:31 +0300

    Create NormalizeSkillCard.jsx

---
+++ src/components/UI/SkillsCard/DependenciesSkills/NormalizeSkillCard/NormalizeSkillCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import logoNormalize from './logo-normalize.svg'
+
+const NormalizeSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+        <div className="skillCard">
+         <img className="skillCard-icon" src={logoNormalize} alt="logo react" />
+         <div className="skillCard-name">Normalize.css</div>
+        </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default NormalizeSkillCard
