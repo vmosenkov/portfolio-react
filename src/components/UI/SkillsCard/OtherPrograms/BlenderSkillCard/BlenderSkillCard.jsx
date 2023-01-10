50f0ce84d0a53dbb3480b1e3459faeef0de64d51
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:06 +0300

    Create BlenderSkillCard.jsx

---
+++ src/components/UI/SkillsCard/OtherPrograms/BlenderSkillCard/BlenderSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoBlender from './logo-blender.svg'
+
+const BlenderSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+         <div className="skillCard">
+            <img className="skillCard-icon" src={logoBlender} alt="logo react" />
+            <div className="skillCard-name">Blender</div>
+         </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default BlenderSkillCard
