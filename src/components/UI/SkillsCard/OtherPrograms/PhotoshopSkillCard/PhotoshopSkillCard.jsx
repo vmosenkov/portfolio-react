42637d46a028a855e749ce5fdd170fb97b0feeca
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:27 +0300

    Create PhotoshopSkillCard.jsx

---
+++ src/components/UI/SkillsCard/OtherPrograms/PhotoshopSkillCard/PhotoshopSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoPhotoshop from './logo-photoshop.svg'
+
+const PhotoshopSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+         <div className="skillCard">
+            <img className="skillCard-icon" src={logoPhotoshop} alt="logo react" />
+            <div className="skillCard-name">Photoshop</div>
+         </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default PhotoshopSkillCard
