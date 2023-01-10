a228d947cb178c378c60bf3a2a8ec009452a56f3
Vladislav <vladmos1995@gmail.com>
2023-01-10 16:56:04 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -7,13 +7,17 @@ const ContactsPage = () => {
             <h1 className="title-1">Contacts</h1>
 
             <ul className="content-list">
-                  <li className="content-list__item">
+                  {/* <li className="content-list__item">
                      <h2 className="title-2">Location</h2>
                      <p>Saint-Petersburg, Russia</p>
-                  </li>
+                  </li> */}
                   <li className="content-list__item">
                      <h2 className="title-2">Telegram</h2>
                      <p><a href="https://t.me/vmosenkov" rel="noreferrer" target="_blank">@vmosenkov</a></p>
+                  </li>
+                  <li className="content-list__item">
+                     <h2 className="title-2">Linkedin</h2>
+                     <p><a href="https://www.linkedin.com/in/vmosenkov/" rel="noreferrer" target="_blank">/vmosenkov</a></p>
                   </li>
                   <li className="content-list__item">
                      <h2 className="title-2">Email</h2>
