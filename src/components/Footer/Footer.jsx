1c16afcd44e19c0f678b01770d93adb241166c83
Vladislav <vladmos1995@gmail.com>
2023-01-10 16:53:56 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 // dependencies - icon
 import { BsInstagram } from 'react-icons/bs';
-import { BsTwitter } from 'react-icons/bs';
+import { BsTelegram } from 'react-icons/bs';
+/* import { BsTwitter } from 'react-icons/bs'; */
 import { BsGithub } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
 import { FaArtstation } from 'react-icons/fa';
@@ -21,13 +22,14 @@ const Footer = () => {
             <ul className="social">
                <li className="social__item"><a href="https://vk.com/vmosenkov" rel="noreferrer" target="_blank"><img src={vk} alt="Link"/></a></li>
                <li className="social__item"><a href="https://www.instagram.com/v.mosenkov/" rel="noreferrer" target="_blank"><BsInstagram className="social__icons" /></a></li>
-               <li className="social__item"><a href="#!" rel="noreferrer" target="_blank"><BsTwitter className="social__icons" /></a></li>
+               <li className="social__item"><a href="https://t.me/vmosenkov" rel="noreferrer" target="_blank"><BsTelegram className="social__icons" /></a></li>
+               {/* <li className="social__item"><a href="#!" rel="noreferrer" target="_blank"><BsTwitter className="social__icons" /></a></li> */}
                <li className="social__item"><a href="https://github.com/vmosenkov" rel="noreferrer" target="_blank"><BsGithub className="social__icons" /></a></li>
-               <li className="social__item"><a href="www.linkedin.com/in/vmosenkov" rel="noreferrer" target="_blank"><BsLinkedin className="social__icons" /></a></li>
+               <li className="social__item"><a href="https://www.linkedin.com/in/vmosenkov/" rel="noreferrer" target="_blank"><BsLinkedin className="social__icons" /></a></li>
                <li className="social__item"><a href="https://www.artstation.com/vmosenkov" rel="noreferrer" target="_blank"><FaArtstation className="social__icons" /></a></li>
             </ul>
             <div className="copyright">
-               {/* <p>All content copyright Vladislav Mosenkov</p> */}
+               <p>©2022 Vladislav Mosenkov</p>
             </div>
             
          </div>
