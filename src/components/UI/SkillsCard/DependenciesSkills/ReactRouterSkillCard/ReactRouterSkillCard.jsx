1641b60b96dde1686dfb7aa9bf31f6ea9d11fceb
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:56:12 +0300

    Create ReactRouterSkillCard.jsx

---
+++ src/components/UI/SkillsCard/DependenciesSkills/ReactRouterSkillCard/ReactRouterSkillCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import logoReactRouterLight from './logo-react_router-light.png'
+
+const ReactRouterSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+        <div className="skillCard">
+         <img className="skillCard-icon" src={logoReactRouterLight} alt="logo react" />
+         <div className="skillCard-name">React Router</div>
+        </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default ReactRouterSkillCard
