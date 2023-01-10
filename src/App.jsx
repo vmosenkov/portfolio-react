3583603ec9c40565d8baf9dbfd3d37be7e5e77c8
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:50:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import React from 'react'
+
+import { Suspense } from 'react';
+
 // styles
 import './styles/main.css';
 
@@ -6,19 +10,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
 // pages
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/Project/ProjectsPage/ProjectsPage';
 import ProjectPage from './pages/Project/ProjectPage/ProjectPage';
 import ContactsPage from './pages/ContactsPage';
+import MySkillsPage from './pages/MySkillsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 // utils
 import ScrollToTop from './utils/scrollToTop.js'
 
+   
+function Loading() {
+   return <h2>🌀 Loading...</h2>;
+}
+
 function App() {
    return (
       <div className="App">
+         <Suspense fallback={<Loading />}>
          <Router>
             <ScrollToTop />
             <Navbar />
@@ -27,9 +39,11 @@ function App() {
                <Route path="/projects" element={<ProjectsPage />} />
                <Route path="/project/:id" element={<ProjectPage />} />
                <Route path="/contacts" element={<ContactsPage />} />
+               <Route path="/myskills" element={<MySkillsPage />} />
                <Route path="*" element={<NotFoundPage />} />
             </Routes>
          </Router>
+         </Suspense>
          <Footer />
       </div>
    );
