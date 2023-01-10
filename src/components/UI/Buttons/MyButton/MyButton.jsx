047df6077aae1bd88024ce28f82696689d1ce232
Vladislav <vladmos1995@gmail.com>
2023-01-10 16:54:55 +0300

    Create MyButton.jsx

---
+++ src/components/UI/Buttons/MyButton/MyButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import cl from './MyButton.module.css';
+
+const MyButton = ({children, ...props}) => {
+	
+	return (
+    	<button {...props} className={cl.myBtn}>{children}</button>
+  	)
+}
+
+export default MyButton;
