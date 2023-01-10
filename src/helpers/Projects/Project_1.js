961eb59e7eab4d451af0bb4d7df48c49a9b85de5
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:53 +0300

    Create Project_1.js

---
+++ src/helpers/Projects/Project_1.js
@@ -0,0 +1,14 @@
+import project_02 from "./../../img/projects/02.jpg";
+import project_02_Big from "./../../img/projects/02-big.jpg";
+
+const project_1 = [
+   {
+      title: 'Video service',
+      skills: 'React, Node.JS, MongoDB',
+      img: project_02,
+      imgBig: project_02_Big,
+      gitHubLink: 'http://github.com/'
+   },
+];
+
+export { project_1 };
