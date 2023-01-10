5a9241bc956193ddfff59e4b0357089393cee1ea
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:57:55 +0300

    Update projectsList.js

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,42 +1,10 @@
-import project_01 from "./../img/projects/01.jpg";
-import project_01_Big from "./../img/projects/01-big.jpg";
-
-import project_02 from "./../img/projects/02.jpg";
-import project_02_Big from "./../img/projects/02-big.jpg";
+import { project_0 } from "./Projects/Project_0/Project_0.js"
 
 const projects = [
    {
-      id:1,
-      title: 'Gaming streaming portal',
-      skills: 'React, Node.JS, MongoDB',
-      img: project_01,
-      imgBig: project_01_Big,
-      gitHubLink: 'http://github.com/'
-   },
-   {
-      id:2,
-      title: 'Video service',
-      skills: 'React, Node.JS, MongoDB',
-      img: project_02,
-      imgBig: project_02_Big,
-      gitHubLink: 'http://github.com/'
-   },
-   {
-      id:3,
-      title: 'Video service',
-      skills: 'React, Node.JS, MongoDB',
-      img: project_02,
-      imgBig: project_02_Big,
-      gitHubLink: 'http://github.com/'
-   },
-   {
-      id:4,
-      title: 'Video service',
-      skills: 'React, Node.JS, MongoDB',
-      img: project_02,
-      imgBig: project_02_Big,
-      gitHubLink: 'http://github.com/'
+      id: 0,
+      project: project_0
    },
 ];
 
-export { projects};
+export { projects };
