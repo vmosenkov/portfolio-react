e30b94db6a764afb260a2e989c9276c698c80851
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:56:46 +0300

    Create HTMLSkillCard.jsx

---
+++ src/components/UI/SkillsCard/LanguagesSkills/HTMLSkillCard/HTMLSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoHTML from './logo-html_5.svg'
+
+const HTMLSkillCard = () => {
+  return ( 
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+        <div className="skillCard">
+          <img className="skillCard-icon" src={logoHTML} alt="logo react" />
+          <div className="skillCard-name">HTML</div>
+        </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default HTMLSkillCard
