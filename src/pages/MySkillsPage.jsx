7870d30a7cd88699e0d26f32978df1adef860383
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:58:21 +0300

    Create MySkillsPage.jsx

---
+++ src/pages/MySkillsPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MySkillsPage = () => {
+
+   return (
+      <main className="section">
+         <div className="container">
+            <h1 className="title-1 ">Skills & <br/> Experience </h1>
+            <ul className="content-list">
+                  <li className="content-list__item">
+                     <h2 className="title-2">Frontend</h2>
+                     <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+                  </li>
+                  <li className="content-list__item">
+                     <h2 className="title-2">Backend</h2>
+                     <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+                  </li>
+            </ul>
+         </div>
+      </main>
+   )
+}
+
+export default MySkillsPage
