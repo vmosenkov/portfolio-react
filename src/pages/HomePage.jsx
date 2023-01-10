8b09b40714632bd40d528c160c5996b66179bfe7
Vladislav <vladmos1995@gmail.com>
2023-01-10 16:56:07 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -34,8 +34,27 @@ const HomePage = () => {
                   </div>
                   <div className="about-content__text">
                      <h2 className="title-2">About Me</h2>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum qui a esse impedit tenetur? Tempora velit aliquid reiciendis rerum consectetur molestiae? Nulla necessitatibus obcaecati vero perferendis eaque quod dignissimos deleniti.</p>
-                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni asperiores deleniti, hic non aperiam ea neque laboriosam, laborum, soluta quis natus unde harum quae. Dolore quas aliquam maiores perferendis natus?</p>
+                     <p>I'm Vladislav Mosenkov, a 27-year-old Front-end developer.</p>
+                     <p>I like to resolve design problems, create smart user interface and imagine useful interaction, developing rich web experiences and web applications.</p>
+                     <p>At one of the last places of work, I created a database with/Knowledge base/Corporate website on CMS Wordpress for employees because it was painful to watch how they work in cloud storage.</p>
+                     <p>Installed the Open Server Panel, installed the website.</p>
+                     <p>Has been implemented:</p>
+                     <ul>
+                        <li className="list-disc">News posts for each department.</li>
+                        <li className="list-disc">The script of the dialogue with the client.</li>
+                        <li className="list-disc">Who is who in the company, etc.</li>
+                     </ul>
+                     <p>During the creation process, I realized that it was necessary to study JavaScript more and began to immerse myself more in the world of frontend development.</p>
+                     <p>Прошел самостоятельно курсы Frontend Developer от компании Glo Academy (04.21 - 10.22).</p>
+                     <ul>
+                        <li className="list-disc">Wordpress для верстальщика</li>
+                        <li className="list-disc">Javascript для верстальщика</li>
+                        <li className="list-disc">Базовый JavaScript</li>
+                        <li className="list-disc">Веб-разработчик 12.0</li>
+                     </ul>
+                     <p>После начал заниматься с ментором по изучению React.</p>
+                     <p>Так же имею опыт в реализации Backend части приложения на стеке React, PHP, Laravel и MySql.</p>
+
                   </div>
                </div>
             </section>
@@ -44,10 +63,16 @@ const HomePage = () => {
                <h2 className="title-2">Technical Skills</h2>
                <div className="main-card">
                   <div className="skills-text">
+                     <h2 className="title-2">Tools</h2>
+                        <p>Использую Windows 10 и Ubuntu в качестве рабочей ОС. Есть опыт использования Figma, Avocode и Zeplin для работы с дизайн-макетами. Использую редактор Visual Studio Code с плагинами Prettier и ESLint для автоматического форматирования и проверки кода.</p>
+                  </div>
+               </div>
+               <div className="main-card">
+                  <div className="skills-text">
                      <h2 className="title-2">Frontend skills</h2>
                      <h2 className="skills-subtitle">Languages</h2>
                      <ul className="skills-row">
-                        <HTMLSkillCard />
+                        <HTMLSkillCard/>
                         <CSSSkillCard />
                         <SassSkillCard />
                         <JavaScriptSkillCard />
@@ -61,6 +86,11 @@ const HomePage = () => {
                      <h2 className="skills-subtitle">Libraries</h2>
                      <ul className="skills-row">
                         <ReactSkillCard />
+                     </ul>
+                     <h2 className="skills-subtitle">Other</h2>
+                     <ul className="skills-row">
+                        <div>Gulp</div>
+                        <div>Webpack</div>
                      </ul>
                   </div>{/* 
                   <div className="divider">
@@ -78,6 +108,8 @@ const HomePage = () => {
                   <div className="skills-text">
                   <h2 className="title-2">Programs/Web Application</h2>
                      <ul className="skills-row">
+                        <div>VS code</div>
+                        <div>Wordpress</div>
                         <GitSkillCard />
                         <PHPMASkillCard />
                      </ul>
