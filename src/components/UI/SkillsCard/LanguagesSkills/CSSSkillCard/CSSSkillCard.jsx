2d00ef38c05484508d9963c22a2f93275b4baaf5
Vladislav <vladmos1995@gmail.com>
2023-01-10 12:56:42 +0300

    Create CSSSkillCard.jsx

---
+++ src/components/UI/SkillsCard/LanguagesSkills/CSSSkillCard/CSSSkillCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// dependencies
+import Tilt from 'react-tilt'
+
+import './../../SkillsCard.css'
+
+import logoCSS from './logo-css_3.svg'
+
+const CSSSkillCard = () => {
+  return (
+   <li>
+      <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
+        <div className="skillCard">
+          <img className="skillCard-icon" src={logoCSS} alt="logo react" />
+          <div className="skillCard-name">CSS</div>
+        </div>
+      </Tilt>
+   </li>
+  )
+}
+
+export default CSSSkillCard
