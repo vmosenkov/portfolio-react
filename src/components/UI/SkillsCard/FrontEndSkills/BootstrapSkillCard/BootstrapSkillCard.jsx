49761918ebcd2e4cfa536ab9b8dd83620435d1fb
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:27:44 +0300

    Update BootstrapSkillCard.jsx

--- src/components/UI/SkillsCard/FrontEndSkills/BootstrapSkillCard/BootstrapSkillCard.jsx
+++ src/components/UI/SkillsCard/FrontEndSkills/BootstrapSkillCard/BootstrapSkillCard.jsx
@@ -12,7 +12,7 @@ const BootstrapSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoBootstrap} alt="logo react" />
+            <img className="skillCard-icon" src={logoBootstrap} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">Bootstrap</div>
          </div>
       </Tilt>
