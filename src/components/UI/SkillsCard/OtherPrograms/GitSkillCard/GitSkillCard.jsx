8f851ccb45c2530a34a2f87829cab59a33812351
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:12 +0300

    Update GitSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/GitSkillCard/GitSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/GitSkillCard/GitSkillCard.jsx
@@ -12,7 +12,7 @@ const GitSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoGit} alt="logo react" />
+            <img className="skillCard-icon" src={logoGit} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">Git</div>
          </div>
       </Tilt>
