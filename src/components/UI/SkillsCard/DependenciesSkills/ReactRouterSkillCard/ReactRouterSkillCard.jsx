6ab24889ca67314c84bc88a71fc29d9ea8974a70
Vladislav <vladmos1995@gmail.com>
2023-01-11 15:33:49 +0300

    Update ReactRouterSkillCard.jsx

--- src/components/UI/SkillsCard/DependenciesSkills/ReactRouterSkillCard/ReactRouterSkillCard.jsx
+++ src/components/UI/SkillsCard/DependenciesSkills/ReactRouterSkillCard/ReactRouterSkillCard.jsx
@@ -10,7 +10,7 @@ const ReactRouterSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoReactRouterLight} alt="logo react" type="image/svg+xml"/>
+         <img className="skillCard-icon" src={logoReactRouterLight} alt="logo react"/>
          <div className="skillCard-name">React Router</div>
         </div>
       </Tilt>
