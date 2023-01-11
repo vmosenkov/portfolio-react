b487fa808a9a42ca8d01cbd8a3a4fe8d413c8b8c
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:00 +0300

    Update SassSkillCard.jsx

--- src/components/UI/SkillsCard/LanguagesSkills/SASSSkillCard/SassSkillCard.jsx
+++ src/components/UI/SkillsCard/LanguagesSkills/SASSSkillCard/SassSkillCard.jsx
@@ -12,7 +12,7 @@ const SassSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoSASS} alt="logo SASS" />
+         <img className="skillCard-icon" src={logoSASS} alt="logo SASS" type="image/svg+xml"/>
          <div className="skillCard-name">SASS</div>
         </div>
       </Tilt>
