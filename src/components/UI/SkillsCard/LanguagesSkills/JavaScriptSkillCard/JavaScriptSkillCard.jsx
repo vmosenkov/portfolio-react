d64c1050bf0491d2e33c43bc2da56481c8618723
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:27:57 +0300

    Update JavaScriptSkillCard.jsx

--- src/components/UI/SkillsCard/LanguagesSkills/JavaScriptSkillCard/JavaScriptSkillCard.jsx
+++ src/components/UI/SkillsCard/LanguagesSkills/JavaScriptSkillCard/JavaScriptSkillCard.jsx
@@ -12,7 +12,7 @@ const JavaScriptSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-            <img className="skillCard-icon" src={logoJS} alt="logo react" />
+            <img className="skillCard-icon" src={logoJS} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">JavaScript</div>
         </div>
       </Tilt>
