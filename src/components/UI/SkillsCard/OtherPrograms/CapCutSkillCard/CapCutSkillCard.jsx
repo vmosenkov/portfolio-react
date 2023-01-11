c520553749bcb73953ffac47bda05202484009b6
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:10 +0300

    Update CapCutSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/CapCutSkillCard/CapCutSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/CapCutSkillCard/CapCutSkillCard.jsx
@@ -12,7 +12,7 @@ const CapCutSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoCapCut} alt="logo react" />
+            <img className="skillCard-icon" src={logoCapCut} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">CapCut</div>
          </div>
       </Tilt>
