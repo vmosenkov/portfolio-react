d6e0fcbd66cddc049a06519fa8f95d58a37e87ba
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:30 +0300

    Update projectsList.js

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,9 +1,14 @@
 import { project_0 } from "./Projects/Project_0/Project_0.js"
+import { project_1 } from "./Projects/Project_1/Project_1.js"
 
 const projects = [
    {
       id: 0,
       project: project_0
+   },
+   {
+      id: 1,
+      project: project_1
    },
 ];
 
