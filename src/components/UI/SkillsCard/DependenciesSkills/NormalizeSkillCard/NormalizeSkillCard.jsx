80f5e82e244ac96798199d45fdd1d933a522ab5e
Vladislav <vladmos1995@gmail.com>
2023-01-11 15:29:21 +0300

    Update NormalizeSkillCard.jsx

--- src/components/UI/SkillsCard/DependenciesSkills/NormalizeSkillCard/NormalizeSkillCard.jsx
+++ src/components/UI/SkillsCard/DependenciesSkills/NormalizeSkillCard/NormalizeSkillCard.jsx
@@ -10,7 +10,7 @@ const NormalizeSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoNormalize} alt="logo react" />
+         <img className="skillCard-icon" src={logoNormalize} alt="logo react" type="image/svg+xml"/>
          <div className="skillCard-name">Normalize.css</div>
         </div>
       </Tilt>
