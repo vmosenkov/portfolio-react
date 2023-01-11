d7f71c442dd89d8c706330f35b5f240a646dedad
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:27:46 +0300

    Update ReactSkillCard.jsx

--- src/components/UI/SkillsCard/FrontEndSkills/ReactSkillCard/ReactSkillCard.jsx
+++ src/components/UI/SkillsCard/FrontEndSkills/ReactSkillCard/ReactSkillCard.jsx
@@ -12,7 +12,7 @@ const ReactSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoReact} alt="logo react" />
+            <img className="skillCard-icon" src={logoReact} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">React</div>
          </div>
       </Tilt>
