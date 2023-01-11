2fbc3eb5ef6308e669a66d89e56b1be0831dff4e
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:05 +0300

    Update BlenderSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/BlenderSkillCard/BlenderSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/BlenderSkillCard/BlenderSkillCard.jsx
@@ -12,7 +12,7 @@ const BlenderSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoBlender} alt="logo react" />
+            <img className="skillCard-icon" src={logoBlender} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">Blender</div>
          </div>
       </Tilt>
