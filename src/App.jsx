23186fab9f525c3f7dbd85744aa86852b5169817
Vladislav <vladmos1995@gmail.com>
2023-01-11 11:59:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,11 @@ import Footer from './components/Footer/Footer';
 
 // pages
 import HomePage from './pages/HomePage';
+import AboutMe from './pages/AboutMe';
 import ProjectsPage from './pages/Project/ProjectsPage/ProjectsPage';
 import ProjectPage from './pages/Project/ProjectPage/ProjectPage';
 import ContactsPage from './pages/ContactsPage';
-/* import MySkillsPage from './pages/MySkillsPage'; */
+import MySkillsPage from './pages/MySkillsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 // utils
@@ -30,10 +31,11 @@ function App() {
             <Navbar />
             <Routes>
                <Route path="/portfolio-react/" element={<HomePage />} />
+               <Route path="/portfolio-react/about-me" element={<AboutMe />} />
                <Route path="/portfolio-react/projects" element={<ProjectsPage />} />
                <Route path="/portfolio-react/project/:id" element={<ProjectPage />} />
                <Route path="/portfolio-react/contacts" element={<ContactsPage />} />
-               {/* <Route path="/portfolio-react/myskills" element={<MySkillsPage />} /> */}
+               <Route path="/portfolio-react/skills" element={<MySkillsPage />} />
                <Route path="*" element={<NotFoundPage />} />
             </Routes>
          </Router>
