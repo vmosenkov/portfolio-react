4cb9d5c562959d89db68d7f4012f566954eb4985
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:28:14 +0300

    Update PhotoshopSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/PhotoshopSkillCard/PhotoshopSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/PhotoshopSkillCard/PhotoshopSkillCard.jsx
@@ -12,7 +12,7 @@ const PhotoshopSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoPhotoshop} alt="logo react" />
+            <img className="skillCard-icon" src={logoPhotoshop} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">Photoshop</div>
          </div>
       </Tilt>
