5dbc89a8b7aef1e95af9774df8c5ca804eeed0d5
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:27:48 +0300

    Update TailwindCSSSkillCard.jsx

--- src/components/UI/SkillsCard/FrontEndSkills/TailwindCSSSkillCard/TailwindCSSSkillCard.jsx
+++ src/components/UI/SkillsCard/FrontEndSkills/TailwindCSSSkillCard/TailwindCSSSkillCard.jsx
@@ -12,7 +12,7 @@ const TailwindCSSSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoTailwindCSS} alt="logo react" />
+            <img className="skillCard-icon" src={logoTailwindCSS} alt="logo react" type="image/svg+xml"/>
             <div className="skillCard-name">TailwindCSS</div>
          </div>
       </Tilt>
