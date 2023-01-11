7bfcae9e8493b9e6d2beacfef14b59abf85a4b77
Vladislav <vladmos1995@gmail.com>
2023-01-11 16:27:42 +0300

    Update StyledComponentSkillCard.jsx

--- src/components/UI/SkillsCard/DependenciesSkills/StyledComponentSkillCard/StyledComponentSkillCard.jsx
+++ src/components/UI/SkillsCard/DependenciesSkills/StyledComponentSkillCard/StyledComponentSkillCard.jsx
@@ -10,7 +10,7 @@ const StyledComponentSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoStyledComponents} alt="logo react" />
+         <img className="skillCard-icon" src={logoStyledComponents} alt="logo react" type="image/svg+xml"/>
          <div className="skillCard-name">Styled component</div>
         </div>
       </Tilt>
