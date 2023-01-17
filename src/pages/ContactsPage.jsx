132b73d1684204694125400b18b92651aa401b87
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:05:19 +0300

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { AiOutlineMail } from "react-icons/ai";
+import ModalMail from '../components/UI/Modal/ModalMail/ModalMail';
 
 const ContactsPage = () => {
+
+   const [isOpen, setIsOpen] = useState(false);
+
    return (
       <main className="section">
          <div className="container">
@@ -20,10 +26,16 @@ const ContactsPage = () => {
                      <p><a href="https://www.linkedin.com/in/vmosenkov/" rel="noreferrer" target="_blank">vmosenkov</a></p>
                   </li>
                   <li className="content-list__item">
+                     <AiOutlineMail size={50} />
                      <h2 className="title-2">Email</h2>
-                     <p><a href="mailto:vladmos1995@gmail.com" rel="noreferrer" target="_blank">vladmos1995@gmail.com</a></p>
+                     <p><a href="mailto:vmosenkov.work@gmail.com" rel="noreferrer" target="_blank">vmosenkov.work@gmail.com</a></p>
                   </li>
             </ul>
+            <div className="contacts__send">
+               <span>Got something for me?</span>
+               <button onClick={() => setIsOpen((isOpen) => !isOpen)}>Send message</button>
+            </div>
+            {isOpen && <ModalMail setIsOpen={setIsOpen}></ModalMail>}
          </div>
       </main>
    )
