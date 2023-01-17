e4fdeb38059d39ff5c2562bc7d64d135c49c9b48
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:54 +0300

    Update PhotoshopSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/PhotoshopSkillCard/PhotoshopSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/PhotoshopSkillCard/PhotoshopSkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // dependencies
 import Tilt from 'react-tilt'
 
-import './../../SkillsCard.css'
+import './../../skillsCard.scss'
 
 import logoPhotoshop from './logo-photoshop.svg'
 
@@ -12,8 +12,8 @@ const PhotoshopSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoPhotoshop} alt="logo react" type="image/svg+xml"/>
-            <div className="skillCard-name">Photoshop</div>
+            <img className="skillCard__icon" src={logoPhotoshop} alt="logo react" type="image/svg+xml"/>
+            <div className="skillCard__name">Photoshop</div>
          </div>
       </Tilt>
    </li>
