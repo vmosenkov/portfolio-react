0cd3fc4ca2792c420d4d525ab694e7375c1ff62d
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:43 +0300

    Update TypeScriptSkillCard.jsx

--- src/components/UI/SkillsCard/LanguagesSkills/TypeScriptSkillCard/TypeScriptSkillCard.jsx
+++ src/components/UI/SkillsCard/LanguagesSkills/TypeScriptSkillCard/TypeScriptSkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // dependencies
 import Tilt from 'react-tilt'
 
-import './../../SkillsCard.css'
+import './../../skillsCard.scss'
 
 import logoTS from './logo-TypeScript.svg'
 
@@ -12,8 +12,8 @@ const TypeScriptSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoTS} alt="logo react" type="image/svg+xml"/>
-         <div className="skillCard-name">TypeScript</div>
+         <img className="skillCard__icon" src={logoTS} alt="logo react" type="image/svg+xml"/>
+         <div className="skillCard__name">TypeScript</div>
         </div>
       </Tilt>
    </li>
