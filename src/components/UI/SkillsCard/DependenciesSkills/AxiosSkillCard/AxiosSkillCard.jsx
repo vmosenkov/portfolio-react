a132e213d91814282e73f45da9a2b0c88969dc82
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:11 +0300

    Update AxiosSkillCard.jsx

--- src/components/UI/SkillsCard/DependenciesSkills/AxiosSkillCard/AxiosSkillCard.jsx
+++ src/components/UI/SkillsCard/DependenciesSkills/AxiosSkillCard/AxiosSkillCard.jsx
@@ -10,8 +10,8 @@ const AxiosSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-         <img className="skillCard-icon" src={logoAxios} alt="logo react" type="image/svg+xml"/>
-         <div className="skillCard-name">Axios</div>
+         <img className="skillCard__icon" src={logoAxios} alt="logo react" type="image/svg+xml"/>
+         <div className="skillCard__name">Axios</div>
         </div>
       </Tilt>
    </li>
