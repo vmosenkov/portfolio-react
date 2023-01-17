b3526dc2f50aef945e930dd523f28e39ddabbb6e
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:39 +0300

    Update JavaScriptSkillCard.jsx

--- src/components/UI/SkillsCard/LanguagesSkills/JavaScriptSkillCard/JavaScriptSkillCard.jsx
+++ src/components/UI/SkillsCard/LanguagesSkills/JavaScriptSkillCard/JavaScriptSkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // dependencies
 import Tilt from 'react-tilt'
 
-import './../../SkillsCard.css'
+import './../../skillsCard.scss'
 
 import logoJS from './logo-js.svg'
 
@@ -12,8 +12,8 @@ const JavaScriptSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
         <div className="skillCard">
-            <img className="skillCard-icon" src={logoJS} alt="logo react" type="image/svg+xml"/>
-            <div className="skillCard-name">JavaScript</div>
+            <img className="skillCard__icon" src={logoJS} alt="logo react" type="image/svg+xml"/>
+            <div className="skillCard__name">JavaScript</div>
         </div>
       </Tilt>
    </li>
