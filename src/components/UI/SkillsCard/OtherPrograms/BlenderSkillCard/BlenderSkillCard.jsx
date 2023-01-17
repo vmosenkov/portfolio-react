59582cf35d0390067981d7a2248abbd7bdff2e1e
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:45 +0300

    Update BlenderSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/BlenderSkillCard/BlenderSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/BlenderSkillCard/BlenderSkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // dependencies
 import Tilt from 'react-tilt'
 
-import './../../SkillsCard.css'
+import './../../skillsCard.scss'
 
 import logoBlender from './logo-blender.svg'
 
@@ -12,8 +12,8 @@ const BlenderSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoBlender} alt="logo react" type="image/svg+xml"/>
-            <div className="skillCard-name">Blender</div>
+            <img className="skillCard__icon" src={logoBlender} alt="logo react" type="image/svg+xml"/>
+            <div className="skillCard__name">Blender</div>
          </div>
       </Tilt>
    </li>
