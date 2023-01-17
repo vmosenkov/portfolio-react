6b0aa74b0c57e5eeea08204a76fc740fe5e68526
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:04:51 +0300

    Update GitSkillCard.jsx

--- src/components/UI/SkillsCard/OtherPrograms/GitSkillCard/GitSkillCard.jsx
+++ src/components/UI/SkillsCard/OtherPrograms/GitSkillCard/GitSkillCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 // dependencies
 import Tilt from 'react-tilt'
 
-import './../../SkillsCard.css'
+import './../../skillsCard.scss'
 
 import logoGit from './logo-git.svg'
 
@@ -12,8 +12,8 @@ const GitSkillCard = () => {
    <li>
       <Tilt className="Tilt" options={{ max : 45, scale: 1.2, perspective: 1000, speed: 1000, transition: true,}}>
          <div className="skillCard">
-            <img className="skillCard-icon" src={logoGit} alt="logo react" type="image/svg+xml"/>
-            <div className="skillCard-name">Git</div>
+            <img className="skillCard__icon" src={logoGit} alt="logo react" type="image/svg+xml"/>
+            <div className="skillCard__name">Git</div>
          </div>
       </Tilt>
    </li>
