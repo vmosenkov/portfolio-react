6f877c8e9d5b3d156f11dddd24f1c34535100fae
Vladislav <vladmos1995@gmail.com>
2023-01-17 16:03:55 +0300

    Update MyButton.jsx

--- src/components/UI/Buttons/MyButton/MyButton.jsx
+++ src/components/UI/Buttons/MyButton/MyButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import cl from './MyButton.module.css';
 
-const MyButton = ({children, ...props}) => {
+const MyButton = ( {children, link, ...props} ) => {
 	
 	return (
-    	<button {...props} className={cl.myBtn}>{children}</button>
+    	<button {...props} className={cl.myBtn}>
+			<a href={ link } className={cl.btn_outline} rel="noreferrer" target="_blank">
+				{children}
+			</a>
+		</button>
   	)
 }
 
